7b957d38c7a8bc668740157e1e67691884da96b0
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-11-16 21:17:09 +0100

    TEst diseñados en una sola función

--- tests.py
+++ tests.py
@@ -4,13 +4,16 @@ from romanos import *
 class RomanosTest(unittest.TestCase):
 
     def test_single_simbol(self):
-        sel.assertEqual(romano_a_entero('M'), 1000)
-        sel.assertEqual(romano_a_entero('D'), 500)
-        sel.assertEqual(romano_a_entero('C'), 100)
-        sel.assertEqual(romano_a_entero('L'), 50)
-        sel.assertEqual(romano_a_entero('X'), 10)
-        sel.assertEqual(romano_a_entero('V'), 5)
-        sel.assertEqual(romano_a_entero('I'), 1)
+        self.assertEqual(romano_a_entero('M'), 1000)
+        self.assertEqual(romano_a_entero('D'), 500)
+        self.assertEqual(romano_a_entero('C'), 100)
+        self.assertEqual(romano_a_entero('L'), 50)
+        self.assertEqual(romano_a_entero('X'), 10)
+        self.assertEqual(romano_a_entero('V'), 5)
+        self.assertEqual(romano_a_entero('I'), 1)
 
-        self.assertRaises(ValueError, romano_a_entero,  'Z')
-        self.assertRaises(ValueError, romano_a_entero,  23)
+        self.assertRaises(ValueError, romano_a_entero, 'Z')
+        self.assertRaises(ValueError, romano_a_entero, 23)
+
+        if __name__ == '__main__':
+            unittest.main()
